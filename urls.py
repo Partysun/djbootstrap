d6a3994eaa11c6c6fb9417cc393428b8667b9af7
Yura Zatsepin <yura.zatsepin@gmail.com>
2012-02-02 00:13:49 +0600

    check

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'djbootstrap.views.home', name='home'),
+    # url(r'^djbootstrap/', include('djbootstrap.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
